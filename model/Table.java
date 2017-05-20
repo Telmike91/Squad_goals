e52b136db96fa5e6559956397d6485f9fc7f33ba
Sumsarg <hetesikrisztian@gmail.com>
2017-05-20 20:01:39 +0200

    flip funkció első verziója implementálva

--- model/Table.java
+++ model/Table.java
@@ -2,12 +2,16 @@ package model;
 //az egész játékmenet központja, ennek a függvényeit hívja meg a control a view műveletei alapján
 //egyelőre úgy van megcsinálva, hogy amint egy sablont a táblára másolt a gép az megszűnik koherens egységként működni
 //nem tudom, hogy ez mennyire jó irány volt, de egyelőre nem akarom újrakezdeni/kijavítani
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.*;
 
 public class Table {
 
     private int height;
     private int width;
     private Field[][] fields;
+	private int rotation;
 
     public Table(int height, int width) {
         this.height = height;
@@ -19,6 +23,8 @@ public class Table {
                 this.fields[i][j] = new Field(i, j);
             }
         }
+		
+		this.rotation = 0;
     }
 
     public void reset() {
@@ -48,6 +54,7 @@ public class Table {
             }
         }
         System.out.println("testing");
+		this.rotation = 0;
         return true;
     }
 
@@ -164,9 +171,173 @@ public class Table {
         }
     }
 
-    public boolean flip() {
-        return false;
+	public boolean flip() {
+        ArrayList<Coordinate> pieceCoords = new ArrayList<Coordinate>();
+		ArrayList<Coordinate> newPieceCoords = new ArrayList<Coordinate>();
+		PieceColor pieceColor = PieceColor.DEFAULT;
+		
+		for (int i = 0; i < this.height; ++i) {
+            for (int j = 0; j < this.width; ++j) {
+                if(fields[i][j].isMoveable()){
+					pieceCoords.add(new Coordinate(fields[i][j].getX(), fields[i][j].getY()));
+					pieceColor = fields[i][j].getColor();
+				}
+            }
+        }
+		
+		switch(pieceColor){
+			case CYAN:
+				switch(rotation){
+					case 0:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+					case 1:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 2:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 3:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+				}
+				break;
+			case BLUE:
+				switch(rotation){
+					case 0:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 1:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 2:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+					case 3:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+				}
+				break;
+			case ORANGE:
+				switch(rotation){
+					case 0:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 1:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+					case 2:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+					case 3:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+				}
+				break;
+			case GREEN:
+				switch(rotation){
+					case 0:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+					case 1:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 2:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 3:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+				}
+				break;
+			case PURPLE:
+				switch(rotation){
+					case 0:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 1:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+					case 2:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+					case 3:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+				}
+				break;
+			case RED:
+				switch(rotation){
+					case 0:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(3));
+						break;
+					case 1:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(2));
+						break;
+					case 2:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(0));
+						break;
+					case 3:
+						newPieceCoords = rotateArray(pieceCoords, pieceCoords.get(1));
+						break;
+				}
+				break;
+			case YELLOW:
+				return true;
+		}
+		
+		try{
+			for(Coordinate coordinate : newPieceCoords){
+				if(!(fields[coordinate.getX()][coordinate.getY()].isEmpty()) && !(fields[coordinate.getX()][coordinate.getY()].isMoveable())){
+					return false;
+				}
+			}
+		}catch(IndexOutOfBoundsException e){
+			return false;
+		}
+		
+		for(Coordinate coordinate : pieceCoords){
+			fields[coordinate.getX()][coordinate.getY()].setFieldState(FieldState.EMPTY);
+			fields[coordinate.getX()][coordinate.getY()].setColor(PieceColor.DEFAULT);
+			fields[coordinate.getX()][coordinate.getY()].stop();
+		}
+		
+		for(Coordinate coordinate : newPieceCoords){
+			fields[coordinate.getX()][coordinate.getY()].setFieldState(FieldState.OCCUPIED);
+			fields[coordinate.getX()][coordinate.getY()].setColor(pieceColor);
+			fields[coordinate.getX()][coordinate.getY()].start();
+		}
+		
+		if(rotation == 3){
+			rotation = 0;
+		}else{
+			rotation += 1;
+		}
+		
+		return true;
     }
+	
+	private ArrayList<Coordinate> rotateArray(ArrayList<Coordinate> pieceCoords, Coordinate around){
+		ArrayList<Coordinate> newPieceCoords = new ArrayList<Coordinate>();
+		
+		for(Coordinate coordinate : pieceCoords){
+			newPieceCoords.add(rotate(coordinate, around));
+		}
+		
+		return newPieceCoords;
+	}
+	
+	private Coordinate rotate(Coordinate what, Coordinate around){
+		Point center = new Point(around.getX(),around.getY());
+		
+		double[] pt = {(double)(what.getX()), (double)(what.getY())};
+		AffineTransform.getRotateInstance(Math.toRadians(90), center.x, center.y)
+		  .transform(pt, 0, pt, 0, 1); // specifying to use this double[] to hold coords
+		double newX = pt[0];
+		double newY = pt[1];
+		
+		return new Coordinate((int)(newX), (int)(newY));
+	}
 
     public int clearRows() {
         int n = 0;
@@ -204,7 +375,7 @@ public class Table {
     }
 
     private void shiftRows(int height) {
-        System.out.println("MAGASSÁG: " + height);
+        System.out.println("MAGASSAG: " + height);
         for (int i = height; i < this.height - 1; i++) {
             for (int j = 0; j < this.width; j++) {
                 fields[i][j].setFieldState(fields[i + 1][j].getFieldState());
